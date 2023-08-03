bd5a8b4b64e055466ca092ebd872302f0841779d
Thinnapat <thinnapat.phopit@gmail.com>
2023-08-03 23:30:39 +0700

    Camera Setup Pos Pending Test

--- Assets/Scripts/Camera/GameSceneSetUPManager.cs
+++ Assets/Scripts/Camera/GameSceneSetUPManager.cs
@@ -13,8 +13,6 @@ namespace Vanaring_DepaDemo
         public List<GameObject> _enemyModelSetupList = new List<GameObject>();
         public List<GameObject> _characterModelSetupList = new List<GameObject>();
 
-        //private int playerindexTest = 0;
-        //private int enemyindexTest = 0;
         private Vector3 rotation = new Vector3(0, 180.0f, 0);
         private bool IsTargetMode = false;
 
@@ -25,6 +23,7 @@ namespace Vanaring_DepaDemo
         public CinemachineVirtualCamera LvirtualCamera;
         public CinemachineVirtualCamera TargetVirtualCamera;
         
+        //Real pos
         private List<GameObject> playerModels = new List<GameObject>();
         private List<GameObject> enemyModels = new List<GameObject>();
         private List<CinemachineVirtualCamera> CamList = new List<CinemachineVirtualCamera>();
@@ -38,7 +37,8 @@ namespace Vanaring_DepaDemo
         private void Awake()
         {
             //myCameraOffset = CameraOffset.LEFT;
-            GenerateModel(playerModels, enemyModels);
+            GeneratePlayerModel(playerModels);
+            GenerateEnemyModel(enemyModels);
         }
         private void Update()
         {
@@ -55,20 +55,42 @@ namespace Vanaring_DepaDemo
             {
                 ToggleTargetMode();
             }
-
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                ReloadAllModelPoint();
+            }
         }
         #region GENERATOR
-        private void GenerateModel(List<GameObject> playerModels, List<GameObject> enemyModels)
+        private void GenerateEnemyModel(List<GameObject> enemyModels)
         {
-            if (_enemyModelSetupList == null || _characterModelSetupList == null)
+            if(_enemyModelSetupList == null)
             {
                 return;
             }
             float totalWidth_e = (_enemyModelSetupList.Count - 1) * spacing;
-            float totalWidth_p = (_characterModelSetupList.Count - 1) * spacing;
             Vector3 startPositionEnemy = transform.position - new Vector3(totalWidth_e * 0.5f, 0f, 0f);
+            //Init Enemy Model
+            for (int i = 0; i < enemyModelSetupList.Count; i++)
+            {
+                Vector3 spawnPosition = startPositionEnemy + new Vector3(i * spacing, 0.5f, 10f);
+                Vector3 targetPosition = startPositionEnemy + new Vector3(i * spacing, 2.5f, 10.5f);
+                GameObject newPoint = Instantiate(enemyModelSetupList[i], spawnPosition, Quaternion.identity, transform);
+                GameObject newTarget = _tgui.Init(targetPosition, newPoint.transform);
+                newPoint.transform.rotation = Quaternion.Euler(rotation);
+                newPoint.name = "Enemy" + i;
+                //enemyPoints.Add(spawnPosition);
+                enemyModels.Add(newPoint);
+                TargetGUIList.Add(newTarget);
+            }
+        }
+        private void GeneratePlayerModel(List<GameObject> playerModels)
+        {
+            if (_characterModelSetupList == null)
+            {
+                return;
+            }
+            float totalWidth_p = (_characterModelSetupList.Count - 1) * spacing;
             Vector3 startPositionPlayer = transform.position - new Vector3(totalWidth_p * 0.5f, 0f, 0f);
-
             //Init PlayerCharacter Model
             for (int j = 0; j < _characterModelSetupList.Count; j++)
             {
@@ -77,32 +99,51 @@ namespace Vanaring_DepaDemo
                 newObject.name = "Player" + j;
                 if (j == 0)
                 {
-                    GenerateVirtualCamera(newObject.transform, CameraOffset.LEFT);
+                    GenerateVirtualCamera(newObject, CameraOffset.LEFT);
                 }
                 else if (j != _characterModelSetupList.Count - 1)
                 {
-                    GenerateVirtualCamera(newObject.transform, CameraOffset.MIDDLE);
+                    GenerateVirtualCamera(newObject, CameraOffset.MIDDLE);
                 }
                 else
                 {
-                    GenerateVirtualCamera(newObject.transform, CameraOffset.RIGHT);
+                    GenerateVirtualCamera(newObject, CameraOffset.RIGHT);
                 }
                 playerModels.Add(newObject);
             }
-            //Init Enemy Model
-            for (int i = 0; i < enemyModelSetupList.Count; i++)
-            {
-                Vector3 spawnPosition = startPositionEnemy + new Vector3(i * spacing, 0.5f, 10f);
-                Vector3 targetPosition = startPositionEnemy + new Vector3(i * spacing, 2.5f, 10.5f);
-                GameObject newObject = Instantiate(enemyModelSetupList[i], spawnPosition, Quaternion.identity, transform);
-                GameObject newTarget = _tgui.Init(targetPosition, newObject.transform);
-                newObject.transform.rotation = Quaternion.Euler(rotation);
-                newObject.name = "Enemy" + i;
-                enemyModels.Add(newObject);
-                TargetGUIList.Add(newTarget);
-            }
         }
-        public void GenerateVirtualCamera(Transform follow, CameraOffset eCam)
+        public void ReloadEnemyModel()
+        {
+            if(enemyModels != null)
+            {
+                foreach (GameObject obj in enemyModels)
+                {
+                    Destroy(obj);
+                }
+                enemyModels.Clear();
+            }
+            Debug.Log("Enemy reloaded");
+            GenerateEnemyModel(enemyModels);
+        }
+        public void ReloadPlayerModel()
+        {
+            if(playerModels != null)
+            {
+                foreach (GameObject obj in playerModels)
+                {
+                    Destroy(obj);
+                }
+                playerModels.Clear();
+            }
+            Debug.Log("Player reloaded");
+            GeneratePlayerModel(playerModels);
+        }
+        public void ReloadAllModelPoint()
+        {
+            ReloadEnemyModel();
+            ReloadPlayerModel();
+        }
+        public void GenerateVirtualCamera(GameObject follow, CameraOffset eCam)
         {
             if(follow == null)
             {
@@ -112,29 +153,29 @@ namespace Vanaring_DepaDemo
             switch (eCam)
             {
                 case CameraOffset.LEFT:
-                    CinemachineVirtualCamera newVCleft = Instantiate(LvirtualCamera, gameObject.transform.position, Quaternion.identity, transform);
+                    CinemachineVirtualCamera newVCleft = Instantiate(LvirtualCamera, gameObject.transform.position, Quaternion.identity, follow.transform);
                     GameObject newAimPointleft = new GameObject("AimPoint");
-                    newAimPointleft.transform.SetParent(transform);
-                    newAimPointleft.transform.position = new Vector3(follow.position.x, follow.position.y, follow.position.z + 10);
-                    newVCleft.Follow = follow;
+                    newAimPointleft.transform.SetParent(follow.transform);
+                    newAimPointleft.transform.position = new Vector3(follow.transform.position.x, follow.transform.position.y, follow.transform.position.z + 10);
+                    newVCleft.Follow = follow.transform;
                     newVCleft.LookAt = newAimPointleft.transform;
                     CamList.Add(newVCleft);
                     break;
                 case CameraOffset.MIDDLE:
-                    CinemachineVirtualCamera newVCmiddle = Instantiate(MvirtualCamera, gameObject.transform.position, Quaternion.identity, transform);
+                    CinemachineVirtualCamera newVCmiddle = Instantiate(MvirtualCamera, gameObject.transform.position, Quaternion.identity, follow.transform);
                     GameObject newAimPointmiddle = new GameObject("AimPoint");
-                    newAimPointmiddle.transform.SetParent(transform);
-                    newAimPointmiddle.transform.position = new Vector3(follow.position.x, follow.position.y, follow.position.z + 10);
-                    newVCmiddle.Follow = follow;
+                    newAimPointmiddle.transform.SetParent(follow.transform);
+                    newAimPointmiddle.transform.position = new Vector3(follow.transform.position.x, follow.transform.position.y, follow.transform.position.z + 10);
+                    newVCmiddle.Follow = follow.transform;
                     newVCmiddle.LookAt = newAimPointmiddle.transform;
                     CamList.Add(newVCmiddle);
                     break;
                 case CameraOffset.RIGHT:
-                    CinemachineVirtualCamera newVCright = Instantiate(RvirtualCamera, gameObject.transform.position, Quaternion.identity, transform);
+                    CinemachineVirtualCamera newVCright = Instantiate(RvirtualCamera, gameObject.transform.position, Quaternion.identity, follow.transform);
                     GameObject newAimPointright = new GameObject("AimPoint");
-                    newAimPointright.transform.SetParent(transform);
-                    newAimPointright.transform.position = new Vector3(follow.position.x, follow.position.y, follow.position.z + 10);
-                    newVCright.Follow = follow;
+                    newAimPointright.transform.SetParent(follow.transform);
+                    newAimPointright.transform.position = new Vector3(follow.transform.position.x, follow.transform.position.y, follow.transform.position.z + 10);
+                    newVCright.Follow = follow.transform;
                     newVCright.LookAt = newAimPointright.transform;
                     CamList.Add(newVCright);
                     break;
@@ -201,6 +242,15 @@ namespace Vanaring_DepaDemo
         {
             get { return _characterModelSetupList; }
             set { _characterModelSetupList = value; }
+        }
+
+        public List<GameObject> GetPlayerModelPointList()
+        {
+            return playerModels;
+        }
+        public List<GameObject> GetEnemyModelPointList()
+        {
+            return enemyModels;
         }
         #endregion
     }
