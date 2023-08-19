4ec61bf1daed0dcacdbf9b7cfb84043dd6b9c6bd
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2023-08-19 20:53:37 +0700

    add main menu

--- Assets/Scripts/Combat/CombatGUI/MainWindowManager.cs
+++ Assets/Scripts/Combat/CombatGUI/MainWindowManager.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing.Design;
 using System.Runtime.InteropServices;
+using TMPro.EditorUtilities;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem.Editor;
 using UnityEngine.UI;
 using Vanaring_DepaDemo;
 
@@ -27,10 +29,11 @@ namespace Vanaring_DepaDemo
 
         private Coroutine _targetingCoroutine = null;
 
+        private Color _hightlightedColor = Color.yellow;
+        private Color _defaultColor;
+
         private void Awake()
         {
- 
-            
             _buttons = new List<Button>();
             if (_itemButton == null || _spellButton == null || _weaponButton == null)
                 throw new Exception("Buttons hasn't been correctly assigned");
@@ -39,7 +42,7 @@ namespace Vanaring_DepaDemo
             _itemButton.onClick.AddListener(() => { _combatGraphicalHandler.DisplayItemPanel(); });
             _spellButton.onClick.AddListener(() => { _combatGraphicalHandler.DisplaySpellPanel(); });
             _weaponButton.onClick.AddListener(() => { _combatGraphicalHandler.DisplayWeaponPanel(); });
-
+            _defaultColor = _spellButton.GetComponent<Image>().color;
             _buttons.Add(_spellButton);
             _buttons.Add(_itemButton);
             _buttons.Add(_weaponButton);
@@ -69,7 +72,8 @@ namespace Vanaring_DepaDemo
 
         public void ReceiveKeys(KeyCode key)
         {
-            int tempWindow = _currentSelectedWindow;
+            UnhightlightButton(_currentSelectedWindow);
+            int tempWindow = _currentSelectedWindow; 
 
             if (key == KeyCode.W)
             {
@@ -111,6 +115,9 @@ namespace Vanaring_DepaDemo
             {
 
             }
+
+
+            HightLightButton(_currentSelectedWindow);
         }
 
         private IEnumerator TargettingTarget()
@@ -147,6 +154,14 @@ namespace Vanaring_DepaDemo
         }
         public override void OnWindowDisplay(CombatGraphicalHandler graophicalHandler)
         {
+            for (int i = 0; i < _buttons.Count; i++)
+            {
+                UnhightlightButton(i); 
+            }
+
+            _currentSelectedWindow = 0; 
+            HightLightButton(_currentSelectedWindow);  
+             
             CentralInputReceiver.Instance().AddInputReceiverIntoStack(this);
             _targetingCoroutine = StartCoroutine(TargettingTarget());
             SetGraphicMenuActive(true);
@@ -155,10 +170,25 @@ namespace Vanaring_DepaDemo
 
         public override void OnWindowOverlayed()
         {
+            for (int i = 0; i < _buttons.Count; i++)
+            {
+                UnhightlightButton(i);
+            }
+
             TargetSelectionFlowControl.Instance.ForceStop();
             CentralInputReceiver.Instance().RemoveInputReceiverIntoStack(this);
 
             SetGraphicMenuActive(false);
+        }
+
+        private void HightLightButton(int index)
+        {
+            _buttons[index].GetComponent<Image>().color = _hightlightedColor;
+        }
+
+        private void UnhightlightButton(int index)
+        {
+            _buttons[index].GetComponent<Image>().color = _defaultColor; 
         }
     }
 }
