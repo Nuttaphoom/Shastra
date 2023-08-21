7a93737682b2702f4325fdf3a8fb1d93cbb650ee
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2023-08-21 12:00:24 +0700

    IDK

--- Assets/Scripts/Combat/CombatGUI/MainWindowManager.cs
+++ Assets/Scripts/Combat/CombatGUI/MainWindowManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing.Design;
 using System.Runtime.InteropServices;
-using TMPro.EditorUtilities;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
