9e0a30ba64be03f684919d401c1d868cad73d793
Thinnapat <thinnapat.phopit@gmail.com>
2023-09-28 21:54:34 +0700

    minor

---
+++ Assets/Scripts/Camera/Timeline/TimelineSignalBroadcaster.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+namespace Vanaring
+{
+    public enum SignalType
+    {
+        SignalA,
+        SignalB,
+        SignalC,
+        SignalD
+    }
+    public class TimelineSignalBroadcaster : MonoBehaviour
+    {
+        
+        public void RecieveSignal(string signal)
+        {
+            switch (signal)
+            {
+                case "A":
+                    Debug.Log("Signal A");
+                    break;
+                //case SignalType.SignalB:
+                //    Debug.Log("Signal B");
+                //    break;
+                //case SignalType.SignalC:
+                //    Debug.Log("Signal C");
+                //    break;
+                //case SignalType.SignalD:
+                //    Debug.Log("Signal D");
+                //    break;
+            }
+        }
+    }
+}
