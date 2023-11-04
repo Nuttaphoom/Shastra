e7e4395c4f11ddb229fae0a301876e765168a44b
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2023-11-04 21:20:17 +0700

    modify POPUPNumber system

--- Assets/Scripts/UI/POPUPNumberTextManager.cs
+++ Assets/Scripts/UI/POPUPNumberTextManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Vanaring
 {
     public class POPUPNumberTextManager : MonoBehaviour
     {
-        public static POPUPNumberTextManager Instance; 
+        public static POPUPNumberTextManager Instance;
+
+        private Dictionary<CombatEntity, List<GameObject>> _spawnedTextDict = new Dictionary<CombatEntity, List<GameObject>>(); 
+        
         private void Awake()
         {
             if (Instance != null)
@@ -16,6 +20,26 @@ namespace Vanaring
             Instance = this; 
         }
 
+        private void Update()
+        {
+            if (Instance != this)
+                Destroy(gameObject);
+
+            foreach (var key in _spawnedTextDict.Keys)
+            {
+                for (int i = 0; i < _spawnedTextDict[key].Count; i++)
+                {
+                    if (_spawnedTextDict[key][i] == null)
+                    {
+                        _spawnedTextDict[key].RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+
+                    _spawnedTextDict[key][i].transform.position = UISpaceSingletonHandler.ObjectToUISpace(key.CombatEntityAnimationHandler.GetGUISpawnTransform());
+                }
+            }
+        }
         [SerializeField]
         private DestroyOnTimer _outputDMGTimerPrefab;
         [SerializeField]
@@ -24,7 +48,6 @@ namespace Vanaring
 
         public void DisplayDPOPUPText(int accumulatedDMG, int accumulatedHP, CombatEntity showToThisEntity )
         {
-            Debug.Log("display");
             if (accumulatedDMG > 0)
             {
                 var v = MonoBehaviour.Instantiate(_outputDMGTimerPrefab, transform);
@@ -32,6 +55,8 @@ namespace Vanaring
                 v.transform.position = UISpaceSingletonHandler.ObjectToUISpace(showToThisEntity.CombatEntityAnimationHandler.GetGUISpawnTransform());
 
                 v.gameObject.SetActive(true);
+
+                AddNewEntityToTextList(showToThisEntity, v.gameObject);
             }
 
             if (accumulatedHP > 0)
@@ -42,7 +67,17 @@ namespace Vanaring
                 
                 vv.gameObject.SetActive(true);
 
+                AddNewEntityToTextList(showToThisEntity, vv.gameObject); 
+
             }
+        }
+
+        private void AddNewEntityToTextList(CombatEntity showToThisEntity, GameObject textGO)
+        {
+            if (!_spawnedTextDict.ContainsKey(showToThisEntity))
+                _spawnedTextDict.Add(showToThisEntity, new List<GameObject>());
+
+            _spawnedTextDict[showToThisEntity].Add(textGO);
         }
     }
 }
