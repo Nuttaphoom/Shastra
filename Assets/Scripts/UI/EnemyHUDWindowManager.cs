d84c7c38826a88f4fc84a2366dfdc8389848e0b0
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2023-11-04 15:04:14 +0700

    EnergyHUD syntax error bugs fixes

--- Assets/Scripts/UI/EnemyHUDWindowManager.cs
+++ Assets/Scripts/UI/EnemyHUDWindowManager.cs
@@ -18,8 +18,7 @@ namespace Vanaring
         //Display what reference(list) want to
         public void DisplayEnemyHUD(List<CombatEntity> entities)
         {
-
-            entities[0].SpellCaster.
+ 
             if(entities.Count < 1)
             {
                 Debug.Log("No enemy on field/detect");
