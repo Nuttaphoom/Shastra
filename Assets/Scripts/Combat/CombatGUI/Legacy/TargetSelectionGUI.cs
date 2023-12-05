96a7f9e91029e00c63cb251c30ac16bbb5e0a01a
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2023-12-05 12:12:19 +0700

    disable circular target icon

--- Assets/Scripts/Combat/CombatGUI/Legacy/TargetSelectionGUI.cs
+++ Assets/Scripts/Combat/CombatGUI/Legacy/TargetSelectionGUI.cs
@@ -16,22 +16,22 @@ namespace Vanaring
         [SerializeField]
         private Dictionary<CombatEntity, GameObject> _instantiatedTargetGUI = new Dictionary<CombatEntity, GameObject>();
 
-        private Dictionary<CombatEntity, GameObject> _instantiatedVFXCircle = new Dictionary<CombatEntity, GameObject>();
+        //private Dictionary<CombatEntity, GameObject> _instantiatedVFXCircle = new Dictionary<CombatEntity, GameObject>();
 
         private Dictionary<CombatEntity, GameObject> _instantiatedBreakGUI = new Dictionary<CombatEntity, GameObject>();
 
 
-        [SerializeField]
-        private GameObject _vfxPrefabTemplate;
+        //[SerializeField]
+        //private GameObject _vfxPrefabTemplate;
 
-        private GameObject instantiatedVFXCircle;
+        //private GameObject instantiatedVFXCircle;
 
         Transform _parent;
 
         public override void Initialize(Transform argc, Null argv = null, Null argg = null)
         {
-            instantiatedVFXCircle = MonoBehaviour.Instantiate(_vfxPrefabTemplate, _vfxPrefabTemplate.transform.position, Quaternion.identity);
-            _instantiatedVFXCircle = new Dictionary<CombatEntity, GameObject>();
+            //instantiatedVFXCircle = MonoBehaviour.Instantiate(_vfxPrefabTemplate, _vfxPrefabTemplate.transform.position, Quaternion.identity);
+            //_instantiatedVFXCircle = new Dictionary<CombatEntity, GameObject>();
             _parent = argc;
 
             SetInit(true);
@@ -40,7 +40,7 @@ namespace Vanaring
         public void HideTargetPointer(CombatEntity combatEntity)
         {
             _instantiatedTargetGUI[combatEntity].SetActive(false);
-            _instantiatedVFXCircle[combatEntity].SetActive(false);
+            //_instantiatedVFXCircle[combatEntity].SetActive(false);
             _instantiatedBreakGUI[combatEntity].SetActive(false);
         }
 
@@ -49,8 +49,8 @@ namespace Vanaring
              foreach (var key in _instantiatedTargetGUI.Keys)
                 _instantiatedTargetGUI[key].SetActive(false);
 
-            foreach (var key in _instantiatedTargetGUI.Keys)
-                _instantiatedVFXCircle[key].SetActive(false);
+            //foreach (var key in _instantiatedTargetGUI.Keys)
+            //    _instantiatedVFXCircle[key].SetActive(false);
             
             foreach (var key in _instantiatedBreakGUI.Keys)
                 _instantiatedBreakGUI[key].SetActive(false);
@@ -69,11 +69,11 @@ namespace Vanaring
                 if (! entities.Contains(key))
                     _instantiatedTargetGUI[key].SetActive(false);
             }
-            foreach (var key in _instantiatedTargetGUI.Keys)
-            {
-                if (! entities.Contains(key))
-                    _instantiatedVFXCircle[key].SetActive(false);
-            }
+            //foreach (var key in _instantiatedTargetGUI.Keys)
+            //{
+            //    if (! entities.Contains(key))
+            //        _instantiatedVFXCircle[key].SetActive(false);
+            //}
             foreach (var key in _instantiatedBreakGUI.Keys)
             {
                 if (!entities.Contains(key))
@@ -92,15 +92,15 @@ namespace Vanaring
                     _instantiatedTargetGUI[combatEntity].SetActive(true);
                 }
 
-                if (!_instantiatedVFXCircle.ContainsKey(combatEntity))
-                {
-                    _instantiatedVFXCircle.Add(combatEntity, MonoBehaviour.Instantiate(_vfxPrefabTemplate, _vfxPrefabTemplate.transform.position, Quaternion.identity));
-                    Vector3 circleTranform = UISpaceSingletonHandler.ObjectToUISpace(combatEntity.CombatEntityAnimationHandler.GetGUISpawnTransform());//  combatEntity.CombatEntityAnimationHandler.GetGUISpawnPos();
-                    _instantiatedVFXCircle[combatEntity].transform.position = new Vector3(circleTranform.x, 0.03f, circleTranform.z);
-                }
+                //if (!_instantiatedVFXCircle.ContainsKey(combatEntity))
+                //{
+                //    _instantiatedVFXCircle.Add(combatEntity, MonoBehaviour.Instantiate(_vfxPrefabTemplate, _vfxPrefabTemplate.transform.position, Quaternion.identity));
+                //    Vector3 circleTranform = UISpaceSingletonHandler.ObjectToUISpace(combatEntity.CombatEntityAnimationHandler.GetGUISpawnTransform());//  combatEntity.CombatEntityAnimationHandler.GetGUISpawnPos();
+                //    _instantiatedVFXCircle[combatEntity].transform.position = new Vector3(circleTranform.x, 0.03f, circleTranform.z);
+                //}
 
-                if (!_instantiatedVFXCircle[combatEntity].activeSelf)
-                    _instantiatedVFXCircle[combatEntity].SetActive(true);
+                //if (!_instantiatedVFXCircle[combatEntity].activeSelf)
+                //    _instantiatedVFXCircle[combatEntity].SetActive(true);
 
                 targetGUI.SetTargetGUIPosition(_instantiatedTargetGUI[combatEntity], UISpaceSingletonHandler.ObjectToUISpace(combatEntity.CombatEntityAnimationHandler.GetGUISpawnTransform()));
             }
