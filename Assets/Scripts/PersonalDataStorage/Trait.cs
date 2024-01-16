580b0d79a9d49b7e470526cae99d2c5a7aa73018
Kuguardt <kuguardt@gmail.com>
2024-01-15 22:35:39 -0800

    Update Personality trait to have a current exp contain with Trait_Data

--- Assets/Scripts/PersonalDataStorage/Trait.cs
+++ Assets/Scripts/PersonalDataStorage/Trait.cs
@@ -15,5 +15,26 @@ namespace Vanaring
             Knowledge,
             Proficiency
         }
+
+        // Trait_Data only contain trait data such as level and currentexp
+        public struct Trait_Data
+        {
+            private int level;
+            public int Getlevel() { return level; }
+            private int currentexp;
+            public int GetCurrentexp() { return currentexp; }
+
+            public Trait_Data(int level, int currentexp)
+            {
+                this.level = level;
+                this.currentexp = currentexp;
+            }
+
+            public void SetTraitData(int level, int currentexp)
+            {
+                this.level = level;
+                this.currentexp = currentexp;
+            }
+        }
     }
 }
