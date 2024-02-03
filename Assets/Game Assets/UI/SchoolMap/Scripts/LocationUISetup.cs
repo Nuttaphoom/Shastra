86cad5ec6d93189ac9e1061540fed1f9ebc635f2
Thinnapat <thinnapat.phopit@gmail.com>
2024-02-03 18:38:29 +0700

    Improve LectureReward UI try to do LectureSelectPanel

--- Assets/Game Assets/UI/SchoolMap/Scripts/LocationUISetup.cs
+++ Assets/Game Assets/UI/SchoolMap/Scripts/LocationUISetup.cs
@@ -18,19 +18,21 @@ namespace Vanaring
         [SerializeField] private TextMeshProUGUI confirmText;
         [SerializeField]
         private SceneDataSO _mapSceneToLoad;
+        [SerializeField]
+        private GameObject _lectureSelectPanel;
+        [SerializeField]
+        private Button tmpLectureButton;
         //[SerializeField] private Button testButton;
         [SerializeField] private List<Transform> buttonTransform = new List<Transform>();
 
         private void Start()
         {
             Init();
+            _lectureSelectPanel = PersistentAddressableResourceLoader.Instance.LoadResourceOperation<GameObject>("LectureSelectPanel");
         }
 
         private void Init()
         {
-            //RuntimeLocation d = PersistentActiveDayDatabase.Instance.GetActiveDayData.GetAvailableLocation()[1];
-            //var loaderDataContainer = (FindObjectOfType<LoaderDataContainer>() ) ;
-
             LoadLocationMenuCommandData currentLocationSO = PersistentSceneLoader.Instance.ExtractLastSavedData<LoadLocationMenuCommandData>().GetData(); //(loaderDataContainer.UseDataInContainer() as LoaderDataUser<LoadLocationMenuCommandData>).GetData() ; //.action_on_this_location;
 
             foreach (var location in PersistentActiveDayDatabase.Instance.GetActiveDayData.GetAvailableLocation()) {
@@ -53,9 +55,18 @@ namespace Vanaring
                     img.sprite = action.GetActionIconSprite;
                 }
                 Button actualButton = newActionButton.GetComponentInChildren<Button>(true);
-                actualButton.onClick.AddListener(() => PerformAction(action));
+                if (action is LectureParticipationActionCommand)
+                {
+                    actualButton.onClick.AddListener(() => PerformAction(action));
+                }
+                if (action is ActivityActionCommand)
+                {
+                    actualButton.onClick.AddListener(() => PerformAction(action));
+                }
+                
                 actualButton.GetComponent<Image>().sprite = action.GetActionIconSprite;  
                 
+                //Set Button Description
                 TextMeshProUGUI[] textComponents = newActionButton.GetComponentsInChildren<TextMeshProUGUI>(true);
                 textComponents[0].text = action.GetActionName;
                 textComponents[1].text = action.GetActionDescription;
@@ -73,11 +84,19 @@ namespace Vanaring
             string ct = string.Format("{0} at the library?\nThis action will consume <color=#FF0000FF>1 time slot</color>, are you sure to perform this action ? ", action.GetActionDescription);
             confirmText.text = ct;
             confirmButton.onClick.AddListener(() => action.ExecuteCommand());
+            
         }
 
         public void OpenPanel()
         {
             confirmPanel.SetActive(true);
+            _lectureSelectPanel.SetActive(true);
+        }
+
+        private void OpenLecturePanel()
+        {
+            Debug.Log("Opn");
+            _lectureSelectPanel.SetActive(true);
         }
 
         public void ClosePanel()
