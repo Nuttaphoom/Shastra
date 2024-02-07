54d0ebe54e378db01779f67f6f8ab5c2d4b7ba5c
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2024-02-07 10:31:16 +0700

    Add CharacterSheeetDatabase

---
+++ Assets/Scripts/Utilities/Database/CharacterSheetDatabaseSO.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
+
+namespace Vanaring
+{
+    [CreateAssetMenu(fileName = "CharacterSheetLocatorSO", menuName = "ScriptableObject/Database/CharacterSheetLocatorSO")]
+    public class CharacterSheetDatabaseSO : BaseDatabaseSO<CharacterSheetSO>
+    {
+        public List<CharacterSheetSO> GetNormalCharacterShhets()
+        {
+            var ret = new List<CharacterSheetSO>(); 
+            foreach (var record in _records) {
+                ret.Add(record.GetRecorded()); 
+            }
+            return ret ;  
+        }
+
+        public List<CombatCharacterSheetSO> GetCombatCharacterShhets()
+        {
+            var ret = new List<CombatCharacterSheetSO>();
+            foreach (var record in _records)
+            {
+                if ( record.GetRecorded() as CombatCharacterSheetSO != null)  
+                    ret.Add(record.GetRecorded() as CombatCharacterSheetSO);
+            }
+            //for (int i = 0; i < ret.Count;  i++)
+            //{
+            //    if (ret[i] == null)
+            //    {
+            //        ret.RemoveAt(i);
+            //        i--; 
+            //    }
+            //}
+             
+            return ret;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/Database/CharacterSheetDatabaseSOEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Vanaring
+{
+    [CustomEditor(typeof(CharacterSheetDatabaseSO))]
+    public class CharacterSheetDatabaseSOEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            CharacterSheetDatabaseSO databaseSO = (CharacterSheetDatabaseSO)target;
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("Actions:");
+
+            // Add a button to call GenerateId()
+            if (GUILayout.Button("Generate ID"))
+            {
+                databaseSO.GenerateId();
+            }
+            if (GUILayout.Button("Remove Duplicated Elements"))
+            {
+                databaseSO.RemoveDuplicatedRecord();
+            }
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("Character Sheets:");
+
+            // Display Normal Character Sheets
+            GUILayout.Label("Normal Character Sheets:");
+            foreach (var characterSheet in databaseSO.GetNormalCharacterShhets())
+            {
+                EditorGUILayout.ObjectField(characterSheet, typeof(CharacterSheetSO), false);
+            }
+
+            GUILayout.Space(10);
+
+            // Display Combat Character Sheets
+            GUILayout.Label("Combat Character Sheets:");
+            foreach (var combatCharacterSheet in databaseSO.GetCombatCharacterShhets())
+            {
+                EditorGUILayout.ObjectField(combatCharacterSheet, typeof(CombatCharacterSheetSO), false);
+            }
+        }
+    }
+}
+ 
