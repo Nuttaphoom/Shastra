b061358936253233a7068fd46efaff18f40e64a9
Thinnapat <thinnapat.phopit@gmail.com>
2024-02-17 21:24:05 +0700

    Daytime Prefabs Update

--- Assets/Scripts/UI/Daytime/DaytimeDisplayer.cs
+++ Assets/Scripts/UI/Daytime/DaytimeDisplayer.cs
@@ -19,6 +19,7 @@ namespace Vanaring
         private int weekDay = 0; //max 6 mon,tue, etc.
         private int month = 0; // max 11
         private int date = 1; //max 29
+        private int time = 0;
 
         private string[] months =
         {
@@ -29,8 +30,8 @@ namespace Vanaring
 
         private string[] days =
         {
-            "Monday", "Tuesday", "Wednesday", "Thursday",
-            "Friday", "Saturday", "Sunday"
+            "Mon", "Tue", "Wed", "Thu",
+            "Fri", "Sat", "Sun"
         };
 
         private string[] dayTime =
@@ -47,9 +48,9 @@ namespace Vanaring
             {
                 weekDay = 0;
             }
-            if (dayCount > 29)
+            if (date > 29)
             {
-                dayCount = 0;
+                date = 0;
                 month++;
             }
             if (month > 11)
@@ -59,17 +60,24 @@ namespace Vanaring
             InitPanel();
             //Debug.Log("Date: " + date + ", Month: " + months[month] + ", Day: " + days[weekDay]);
         }
+        public void AdvanceDayNight()
+        {
+            time++;
+        }
 
         private void Awake()
         {
             InitPanel();
-            Debug.Log("Date: " + date + ", Month: " + months[month] + ", Day: " + days[weekDay]);
+            
+            //Debug.Log(PersistentActiveDayDatabase.Instance.GetDayProgressionHandler.GetCurrentDayTime());
         }
 
+        [ContextMenu("ShowPanelDetail")]
         private void InitPanel()
         {
             dateText.text = date + "/" + months[month] + " " + days[weekDay];
             dayTimeText.text = dayTime[PersistentActiveDayDatabase.Instance.GetDayProgressionHandler.GetCurrentDayTime()].ToString();
+            Debug.Log("Date: " + date + ", Month: " + months[month] + ", Day: " + days[weekDay]);
         }
 
         public void OpenPanel(){
