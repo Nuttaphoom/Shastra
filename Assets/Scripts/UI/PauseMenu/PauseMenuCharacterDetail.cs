a2a3b7dca52ac351e8b574fc350539adcb9c370b
Thinnapat <thinnapat.phopit@gmail.com>
2024-03-25 22:21:59 +0700

    update pause menu

--- Assets/Scripts/UI/PauseMenu/PauseMenuCharacterDetail.cs
+++ Assets/Scripts/UI/PauseMenu/PauseMenuCharacterDetail.cs
@@ -10,14 +10,17 @@ namespace Vanaring
     public class PauseMenuCharacterDetail : PauseMenuWindowGUI
     {
         [SerializeField] private Image characterPortrait;
+        [SerializeField] private Image characterPortraitBG;
         [SerializeField] private Image hpFillBar;
         [SerializeField] private Image mpFillBar;
+        [SerializeField] private TextMeshProUGUI atkStat;
         [SerializeField] private TextMeshProUGUI characterName;
         [SerializeField] private TextMeshProUGUI characterDescription;
         [SerializeField] private GameObject gfx;
         [SerializeField] private GameObject spellPanel;
         private List<SpellActionSO> spellRegiteredList = new List<SpellActionSO>();
         private List<CombatCharacterSheetSO> combatCharacterSheet = new List<CombatCharacterSheetSO>();
+        private int characterIndex = 0;
         public override void ClearData()
         {
             
@@ -27,21 +30,18 @@ namespace Vanaring
         {
             CharacterSheetDatabaseSO charactersheetDataSO = PersistentAddressableResourceLoader.Instance.LoadResourceOperation<CharacterSheetDatabaseSO>(DatabaseAddressLocator.GetCharacterSheetDatabaseAddress);
             combatCharacterSheet = charactersheetDataSO.GetCombatCharacterShhets();
-            foreach (CombatCharacterSheetSO character in combatCharacterSheet)
-            {
-                hpFillBar.fillAmount = entity.StatsAccumulator.GetHPAmount() / entity.StatsAccumulator.GetPeakHPAmount();
-            }
+
+            hpFillBar.fillAmount = 1.0f;
+            mpFillBar.fillAmount = 1.0f;
+            atkStat.text = combatCharacterSheet[0].GetATK.ToString();
+            characterPortrait.sprite = combatCharacterSheet[0].GetCharacterIcon;
+            characterPortraitBG.sprite = combatCharacterSheet[0].GetCharacterIcon;
 
             //characterName.text = runtimePartyPear.GetMemberName;
             //runtimePartyAsha.GetCharacterSheet.
             //hpFillBar.fillAmount = entity.StatsAccumulator.GetHPAmount() / entity.StatsAccumulator.GetPeakHPAmount();
             //mpFillBar.fillAmount = entity.SpellCaster.GetMP / entity.SpellCaster.GetPeakMP;
             //characterPortrait.sprite = entity.CombatCharacterSheet.GetCharacterIcon;
-        }
-
-        public void LoadWindowData()
-        {
-            
         }
 
         public override void OnWindowActive()
@@ -63,8 +63,36 @@ namespace Vanaring
             }
             else if (key == KeyCode.Q)
             {
-                
+                //if 
+                //{
+
+                //}
             }
+            else if (key == KeyCode.W)
+            {
+                if (characterIndex > 0)
+                {
+                    characterIndex--;
+                }
+                SwitchCharacter();
+            }
+            else if (key == KeyCode.S)
+            {
+                if (characterIndex < combatCharacterSheet.Count - 1)
+                {
+                    characterIndex++;
+                }
+                SwitchCharacter();
+            }
+        }
+
+        private void SwitchCharacter()
+        {
+            hpFillBar.fillAmount = 1.0f;
+            mpFillBar.fillAmount = 1.0f;
+            atkStat.text = combatCharacterSheet[characterIndex].GetATK.ToString();
+            characterPortrait.sprite = combatCharacterSheet[characterIndex].GetCharacterIcon;
+            characterPortraitBG.sprite = combatCharacterSheet[characterIndex].GetCharacterIcon;
         }
     }
 }
