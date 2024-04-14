614f3c24203407edcb6b0bb0397f7a032a41d33e
Thinnapat <thinnapat.phopit@gmail.com>
2024-04-14 16:20:10 +0700

    update prototype walk animation in mission

--- Assets/Scripts/Dungeon/MissionPartyAnimationManager.cs
+++ Assets/Scripts/Dungeon/MissionPartyAnimationManager.cs
@@ -6,16 +6,50 @@ namespace Vanaring
 {
     public class MissionPartyAnimationManager : MonoBehaviour
     {
-        private Animator animator;
+        [SerializeField] private List<Animator> animatorList = new List<Animator>();
 
-        private void Start()
+        public void PerformMoveAnimation(Vector3 direction)
         {
-            animator.GetComponent<Animator>();
+            foreach (Animator anim in animatorList)
+            {
+                anim.SetBool("IsMoving", true);
+                anim.SetFloat("horizontalMove", direction.x);
+                anim.SetFloat("verticalMove", direction.z);
+            }
         }
 
-        public void PerformMove()
+        public void PerformIdleAnimation()
         {
-            animator.SetFloat("CharacterMovement", 1.0f);
+            
+            foreach (Animator anim in animatorList)
+            {
+                anim.SetBool("IsMoving", false);
+                //anim.SetFloat("horizontalMove", 0.0f);
+                //anim.SetFloat("verticalMove", 0.0f);
+            }
+        }
+
+        public IEnumerator OnCharacterMoveToNode(BaseMissionNode nodeToVisit)
+        {
+            Vector3 direction = (nodeToVisit.transform.position - transform.position).normalized;
+            Debug.Log("Direction - x: " + direction.x + ", y: " + direction.y + ", z: " + direction.z);
+
+            Vector3 prevUIPos = transform.position;
+            float progression = 0;
+
+            PerformMoveAnimation(direction);
+
+            while (progression < 1)
+            {
+
+                transform.position = Vector3.Lerp(prevUIPos, nodeToVisit.transform.position, progression);
+                progression += Time.deltaTime / 1.7f;
+
+                yield return null;
+
+            }
+
+            PerformIdleAnimation();
         }
     }
 }
