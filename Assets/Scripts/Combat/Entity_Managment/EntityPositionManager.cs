5f379a2a7ae678bf304417987037923e1308d033
Thinnapat <thinnapat.phopit@gmail.com>
2024-05-07 00:01:32 +0700

    build setup

--- Assets/Scripts/Combat/Entity_Managment/EntityPositionManager.cs
+++ Assets/Scripts/Combat/Entity_Managment/EntityPositionManager.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine; 
 
 namespace Vanaring
