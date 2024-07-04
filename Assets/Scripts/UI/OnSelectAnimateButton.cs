e556cfcbfa89fd104cf63a5a24db3c7ad828cde0
Thinnapat <thinnapat.phopit@gmail.com>
2024-07-04 17:36:56 +0700

    add sprite for turn switch, arrow in dungeon animation

---
+++ Assets/Scripts/UI/OnSelectAnimateButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vanaring
+{
+    public class OnSelectAnimateButton : MonoBehaviour
+    {
+        [SerializeField] private Animator buttonAnim;
+        [SerializeField] private string selectAnimationName;
+        [SerializeField] private string deselectAnimationName;
+
+        public void OnSelect()
+        {
+            buttonAnim.Play(selectAnimationName);
+        }
+
+        public void OnDeselect()
+        {
+            buttonAnim.Play(deselectAnimationName);
+        }
+    }
+}
