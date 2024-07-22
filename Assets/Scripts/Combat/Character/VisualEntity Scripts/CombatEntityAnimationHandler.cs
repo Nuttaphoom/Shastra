f23dbebf8615cdad89ae997620ad90456cbbc121
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2024-07-22 11:27:54 +0700

    adjust hit animation

--- Assets/Scripts/Combat/Character/VisualEntity Scripts/CombatEntityAnimationHandler.cs
+++ Assets/Scripts/Combat/Character/VisualEntity Scripts/CombatEntityAnimationHandler.cs
@@ -447,16 +447,20 @@ namespace Vanaring
         }
         public IEnumerator PlaySpawnVisualEffectCoroutine()
         {
+            HideVisualMesh(); 
+
             float overallTime = 0.0f;
             if (_spawnVisualEffect != null)
             {
                 _spawnVisualEffect.gameObject.SetActive(true);
                 _spawnVisualEffect.Play();
-                overallTime = _spawnVisualEffect.main.duration;
+                overallTime =  1.5f ;
+
+                Debug.Log("overallTime " + overallTime); 
                 yield return new WaitForSeconds(overallTime / 2);
             }
 
-            _visualMesh.SetActive(true);
+            ShowVisualMesh(); 
 
             yield return new WaitForSeconds(overallTime / 2);
 
@@ -464,6 +468,8 @@ namespace Vanaring
             {
                 _spawnVisualEffect.gameObject.SetActive(false);
             }
+
+            Debug.Log("spawn visual effect end");
         }
         #endregion
 
