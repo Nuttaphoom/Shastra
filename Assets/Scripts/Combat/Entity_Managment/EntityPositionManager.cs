247ed65d2ad07a05eea52e3b3d01cf1b3036c91d
Nuttaphoom Boonmee <nuttaphoomboonmee@hotmail.com>
2024-07-25 22:36:35 +0700

    add Charge animation for boss fix general bugs

--- Assets/Scripts/Combat/Entity_Managment/EntityPositionManager.cs
+++ Assets/Scripts/Combat/Entity_Managment/EntityPositionManager.cs
@@ -199,8 +199,6 @@ namespace Vanaring
             List<StandingLocationOccupierData> oldLocationData = GetEnemyStandingLocations(_currentEnemySize);
 
             _currentEnemySize = newSize;
-
-
             
             int i = 0;
 
@@ -248,14 +246,18 @@ namespace Vanaring
             {
 
                 var enemyOccupation = GetEnemyStandingLocations(CurrentEnemySize);
+
                 if (enemyOccupation[index].EntityStandingHere == null || enemyOccupation[index].EntityStandingHere == entity)
                 {
                     enemyOccupation[index].EntityStandingHere = entity;
                     data = enemyOccupation[index];
-                }else
-                {
-                    throw new Exception( entity.gameObject.name + "Try to force standing on the occupied location || index is " + index + " with current enemy size " + CurrentEnemySize + " and  " + enemyOccupation[index].EntityStandingHere + " is standing here") ;
                 }
+                else
+                {
+                    throw new Exception(entity.gameObject.name + "Try to force standing on the occupied location || index is " + index + " with current enemy size " + CurrentEnemySize + " and  " + enemyOccupation[index].EntityStandingHere + " is standing here");
+                }
+
+                
 
             }
 
@@ -413,11 +415,14 @@ namespace Vanaring
                 }
             }
 
-            foreach (var data in GetEnemyStandingLocations(_currentEnemySize))
+            for (int i = 0; i < 10; i++)
             {
-                if (data.EntityStandingHere == entity)
+                foreach (var data in GetEnemyStandingLocations(i))
                 {
-                    return data ;
+                    if (data.EntityStandingHere == entity)
+                    {
+                        return data;
+                    }
                 }
             }
 
